05840248c96a2fe4903321ad91d9c54d883b102d
henrytrust1111 <henrytrust1111@gmail.com>
2025-05-26 14:41:32 +0100

    latest push

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,17 +1,11 @@
-import Chart from '@/components/dashboard/chart/Chart'
-import Container from '@/components/dashboard/Container'
-import Orders from '@/components/dashboard/orders/Orders'
 import React from 'react'
 
-const Page = () => {
+const page = () => {
   return (
-  <>
-   <Container>
-    <Chart />
-    <Orders />
-   </Container>
-  </>
+    <div>
+      
+    </div>
   )
 }
 
-export default Page
+export default page
